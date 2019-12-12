8aca03812deb9ccd5c3bcb0ac95f25fc637c15c9
kellycoles <kellycoles1@gmail.com>
2019-12-12 04:45:52 -0600

    Add project files.

---
+++ MowPro/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MowPro.Models;
+
+namespace MowPro.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Job> Job { get; set; }
+        public DbSet<Service> Service { get; set; }
+        public DbSet<CustomerService> CustomerService { get; set; }
+    }
+}
